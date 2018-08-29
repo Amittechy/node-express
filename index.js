2565956393e2e9cb601a5a965aff265dfc8cb289
Amit Rai <techamitn@gmail.com>
2018-08-29 21:44:15 +0530

    Express Serves Static Files

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express'),
 http = require('http');
+const morgan = require(morgan);
+
 const hostname="localhost";
+
 const port = 3000;
 const app = express();
 
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
 app.use((req, res, next) => {
     console.log(req.headers);
     res.statusCode = 200;
@@ -17,4 +22,3 @@ app.use((req, res, next) => {
   server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
   });
-  
